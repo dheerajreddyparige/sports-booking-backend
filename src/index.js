40b724927e27049e2606f55539beaf5c29263c01
pdheerajreddy13 <pdheerajreddy13@gmail.com>
2025-04-24 16:24:52 +0100

    before start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const connectToDatabase = require('./lib/connect-to-database');
+const connectToDatabase = require('./util/connect-to-database');
 require('dotenv').config();
 
 const app = express();
@@ -13,10 +13,11 @@ connectToDatabase()
   .catch(err => console.error('MongoDB connection failed:', err));
 
 // Routes
-app.use('/api/health', require('./api/health'));
-app.use('/api/available-slots', require('./api/available-slots'));
-app.use('/api/bookings', require('./api/bookings'));
-app.use('/api/sports', require('./api/sports'));
+app.use('/api/health', require('../src/api/health'));
+app.use('/api/available-slots', require('../src/api/available-slots'));
+app.use('/api/bookings', require('../src/api/bookings'));
+app.use('/api/sports', require('../src/api/sports'));
+app.use('/', require('../src/controllers/flowscontroller'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
